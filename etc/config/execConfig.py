e5f93ed7eb4728da8016f63ffb9a2f7a84ab07ef
Stephen R. Pietrowicz <srp@ncsa.illinois.edu>
2016-12-02 13:25:57 -0600

    Additional configuration changes for workspace directories, addition of NODE_SET removal of extraneous commented out configuration information

--- etc/config/execConfig.py
+++ etc/config/execConfig.py
@@ -1,5 +1,7 @@
-config.platform.localScratch = "/scratch/srp/condor_scratch"
-config.platform.defaultRoot = "/lsst/DC3root"
+config.platform.nodeSetRequired = True
+config.platform.localScratch = "/scratch/$USER_NAME/condor_scratch"
+config.platform.defaultRoot = "/scratch/$USER_NAME/condor_work"
 config.platform.dataDirectory = "/lsst7/stripe82/dr7/runs"
 config.platform.fileSystemDomain = "ncsa.illinois.edu"
 config.platform.scheduler = "slurm"
+config.platform.setup_using = "getenv"
