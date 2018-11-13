05368c8563ce5ef13f7cd68cda20e7788f9ae887
Jeff Carlin <jcarlin@lsst.org>
2018-11-13 18:33:06 +0000

    Enable flake8 testing (probably unnecessarily for this repo).

---
+++ tests/test_math.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+def add(x, y):
+    return x + y
+
+
+class MyTest(unittest.TestCase):
+    def test(self):
+        self.assertEqual(add(3, 4), 7)
+
+
+if __name__ == '__main__':
+    unittest.main()
