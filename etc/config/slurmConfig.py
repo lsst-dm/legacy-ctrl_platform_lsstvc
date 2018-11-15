9035d6faf2f6f21566cf710de2a9aa126ebccdba
Stephen R Pietrowicz <srp@illinois.edu>
2018-11-15 15:45:16 -0600

    Additional information about what allocateNodes does when labeling nodes and information about what you'll see on successful allocation Changed "queue" so it can be taken from the command line of allocateNodes.py

--- etc/config/slurmConfig.py
+++ etc/config/slurmConfig.py
@@ -1,4 +1,4 @@
-config.platform.queue = "debug"
+config.platform.queue = "$QUEUE"
 config.platform.scratchDirectory = "/scratch/$USER_NAME/condor_scratch"
 config.platform.loginHostName = "lsst-dev.ncsa.illinois.edu"
 config.platform.utilityPath = "/usr/sbin/"
